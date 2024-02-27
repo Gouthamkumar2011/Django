31f709856cdc8738b030330e7fc781a5b86dea13
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2024-02-27 12:01:57 -0500

    Day_2_Portfolio_Building

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+rooms = [
+    {'id': 1,'name':'Lets learn python'},
+    {'id': 2,'name':'Design Django'},
+    {'id': 3,'name':'Develop REST API'}
+]
+
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'base/home.html',{'rooms': rooms})
 
-def room(request):
-    return HttpResponse("ROOM")
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html',context)
